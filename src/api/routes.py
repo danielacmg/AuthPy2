318ee6ccad06c0d2b521f9afcc5974d88eb421e3
danielacmg <dcmg.ucv@gmail.com>
2023-04-01 21:28:48 +0000

    first commit

--- src/api/routes.py
+++ src/api/routes.py
@@ -5,6 +5,10 @@ from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
 
+from flask_jwt_extended import create_access_token
+from flask_jwt_extended import get_jwt_identity
+from flask_jwt_extended import jwt_required
+
 api = Blueprint('api', __name__)
 
 
@@ -16,3 +20,46 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/whatever', methods=['GET'])
+def testing_route():
+
+    response_body = {
+        "message": "NO entiendo Nada!"
+    }
+
+    return jsonify(response_body), 200
+
+##################### Create User #####################################
+@api.route('/user', methods=['POST'])
+def create_user():
+    # First we get the payload json
+    body = request.get_json()    
+    
+    if body is None:
+        raise APIException("You need to specify the request body as a json object", status_code=400)
+    if 'email' not in body:
+        raise APIException('You need to specify the email', status_code=400)
+    if 'password' not in body:
+        raise APIException('You need to specify the password', status_code=400)
+    
+    # to inster the user into the bd
+    new_user = User(password=body['password'], email=body['email'], is_active=True)
+    db.session.add(new_user)
+    db.session.commit()
+    return "User created", 200
+
+############################ Login/Create token ################################################
+# Create a route to authenticate your users and return JWTs. The
+# create_access_token() function is used to actually generate the JWT.
+@api.route("/login", methods=["POST"])
+def user_login():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    if email != "test" or password != "test":
+        return jsonify({"msg": "Bad email or password"}), 401
+
+    access_token = create_access_token(identity=email)
+    return jsonify(access_token=access_token)
+
+#################################################
--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,18 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+  <footer className="footer mt-auto py-3 text-center">
+    <p>
+      Made by{" "}
+      <a
+        className="texttext-decoration-none"
+        href="https://github.com/danielacmg"
+        data-toggle="tooltip"
+        data-placement="bottom"
+        title="go to my github page"
+      >
+        d
+      </a>
+    </p>
+  </footer>
 );
